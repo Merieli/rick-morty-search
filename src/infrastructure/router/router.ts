be865b35412e6bb4d422f67dc536f856a0e890da
Merieli Manzano <merieli.amanzano@gmail.com>
2023-06-16 13:04:10 -0300

    feat: test to add metaTags in page

--- src/infrastructure/router/router.ts
+++ src/infrastructure/router/router.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-// These can be imported from other files
-// const Home = { template: '<div>Home</div>' }
-// const About = { template: '<div>About</div>' }
+import PageHomeVue from '@/views/PageHome.vue';
 
 // 2. Define some routes
 // Each route should map to a component.
@@ -10,9 +8,28 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
     {
         path: '/',
+        component: PageHomeVue,
+        meta: {
+            title: "",
+            metaTags: [
+                {
+                  name: 'description',
+                  content: 'Bem-vindo à página inicial'
+                },
+                {
+                    name: 'Outro',
+                    content: 'Algo'
+                },
+                {
+                    property: 'og:description',
+                    content: 'Bem-vindo à página inicial - OpenGraph'
+                }
+            ]
+        }
     },
     //   { path: '/about', component: About },
 ];
+
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
@@ -20,4 +37,32 @@ const routes = [
 export const router = createRouter({
     history: createWebHashHistory(),
     routes,
+});
+
+
+// Antes de cada rota ser renderizada, atualize as meta-tags
+router.beforeEach((to, from, next) => {
+    // Defina o título da página
+    document.title = to.meta.title as string  || 'Meu Site';
+  
+    // Remova as meta-tags antigas
+    const {head} = document;
+    const metaTags = head.getElementsByTagName('meta');
+    for (let i = metaTags.length - 1; i >= 0; i--) {
+        head.removeChild(metaTags[i]);
+    }
+  
+    // Adicione as novas meta-tags
+    if (to.meta.metaTags) {
+        to.meta.metaTags.forEach((tag: any) => {
+            const metaTag = document.createElement('meta');
+            Object.entries(tag).forEach(([key, value]) => {
+                metaTag.setAttribute(key, value as string);
+            });
+            head.appendChild(metaTag);
+        });
+    }
+  
+    // Continue para a próxima rota
+    next();
 });
