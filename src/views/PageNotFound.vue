7b19ff5803d3508aa19ec0a9e6f9453fc1c24568
Merieli Manzano <merieli.amanzano@gmail.com>
2023-07-17 21:31:47 -0300

    feat: add page notfound

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({});
+</script>
+
+<template>
+    <section>
+        <h1>Ops, esta página não foi encontrada</h1>
+    </section>
+</template>
+
+<style lang="postcss" scoped></style>
